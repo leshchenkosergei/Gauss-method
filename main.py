b3d4fcf6f73667e953326f2988f8e75ab9e767f9
Sergei Leshchenko <leshchenko.sergei15@gmail.com>
2022-02-23 00:45:14 +0300

    Вроде бы строится треугольник

--- main.py
+++ main.py
@@ -23,11 +23,33 @@ def keyboard_read():
         matrix.append(str)
     return matrix
 
+def transform(matrix):
+    for i in range(len(matrix) - 1):
+
+        max = abs(matrix[i][i])
+        el_num = 0
+        for j in range(i, len(matrix)):
+            if abs(matrix[j][i]) > max:
+                max = matrix[j][i]
+                el_num = j
+
+        if (max > abs(matrix[i][i])):
+            row = matrix[i]
+            matrix[i] = matrix[el_num]
+            matrix[el_num] = row
+
+        for k in range(i + 1, len(matrix)):
+            с = matrix[k][i]/matrix[i][i]
+            for j in range(i, len(matrix) + 1):
+                matrix[k][j] = round(matrix[k][j] - с * matrix[i][j], 3)
+
+    return matrix
+
 def main_fun():
     print("Введите \"1\" для ввода с клавиатуры, введите \"2\" для чтения из файла")
     num = input()
     if num == "1":
-        print(keyboard_read())
+        print(transform(keyboard_read()))
 
 about_lab()
 main_fun()
