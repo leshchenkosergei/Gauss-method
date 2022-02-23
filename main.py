33b3a6808f2378e682376a6d68fe8fd3e12511d2
Sergei Leshchenko <leshchenko.sergei15@gmail.com>
2022-02-23 19:47:49 +0300

    Считаются корни, сделал красивый вывод матрицы

--- main.py
+++ main.py
@@ -4,6 +4,13 @@ def about_lab():
     print("Автор: Лещенко Сергей Дмитриевич P3223")
     print()
 
+def get_count(number):
+    s = str(number)
+    if '.' in s:
+        return abs(s.find('.') - len(s)) - 1
+    else:
+        return 0
+
 def keyboard_read():
     n = 0
     while True:
@@ -22,6 +29,7 @@ def keyboard_read():
         str = list(map(float, line.split()))
         matrix.append(str)
     return matrix
+
 
 def transform(matrix):
     for i in range(len(matrix) - 1):
@@ -45,11 +53,36 @@ def transform(matrix):
 
     return matrix
 
+
+def get_roots(matrix):
+    result = [0] * len(matrix)
+    for i in range(len(matrix) - 1, -1, -1):
+        s = 0
+        for j in range(i + 1, len(matrix)):
+            s = s + matrix[i][j] * result[j]
+        result[i] = round((matrix[i][len(matrix)] - s) / matrix[i][i], 3)
+    return result
+
+
 def main_fun():
     print("Введите \"1\" для ввода с клавиатуры, введите \"2\" для чтения из файла")
     num = input()
     if num == "1":
-        print(transform(keyboard_read()))
+        matrix = transform(keyboard_read())
+    str_matrix = ""
+    for i in range(0, len(matrix)):
+        for j in range(0, len(matrix)):
+            str_matrix = str_matrix + matrix[i][j].__str__()
+            if get_count(matrix[i][j]) == 1:
+                str_matrix = str_matrix + "   "
+            elif get_count(matrix[i][j]) == 2:
+                str_matrix = str_matrix + "  "
+            else:
+                str_matrix = str_matrix + " "
+        str_matrix = str_matrix + '\n'
+    print("Преобразованная матрица со столбцом B:")
+    print(str_matrix)
+    print()
 
 about_lab()
 main_fun()
